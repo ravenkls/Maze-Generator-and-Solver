c474e33edb01483cb99c92bf98c4458454fe993c
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-14 20:18:18 -0800

    Optimization to quadruple speed.
    I removed all the unnecessary numpy code (eg. conversion to tuple from list) which added a lot of overhead. I also directly import random.choice, and removed checks against cells.shape - we already know that the shape is row x columns.
    Additionally, you can call np.zeros(size) directly, without needing to reshape it.

--- maze_generator.py
+++ maze_generator.py
@@ -4,42 +4,44 @@
 from queue import LifoQueue
 from PIL import Image
 import numpy as np
-import random
+from random import choice
 
 def generate_maze(rows, columns):
-    maze = Image.new('RGB', (2*columns+1, 2*rows+1), 'black')
+    maze = Image.new('RGB', (2 * columns + 1, 2 * rows + 1), 'black')
     pixels = maze.load()
 
     # Create a path on the very top and bottom so that it has an entrance/exit
     pixels[1, 0] = (255, 255, 255)
     pixels[-2, -1] = (255, 255, 255)
-
     stack = LifoQueue()
-    cells = np.zeros(rows*columns).reshape(rows, columns)
+    cells = np.zeros((rows, columns))
     cells[0, 0] = 1
     stack.put((0, 0))
 
     while not stack.empty():
         x, y = stack.get()
-        
-        left = (x-1, y) if x > 0 and cells[x-1, y] == 0 else None
-        right = (x+1, y) if x < cells.shape[0]-1 and cells[x+1, y] == 0 else None
-        top = (x, y-1) if y > 0 and cells[x, y-1] == 0 else None
-        bottom = (x, y+1) if y < cells.shape[1]-1 and cells[x, y+1] == 0 else None
-        
-        adjacents = np.array([left, right, top, bottom])
-        unvisited_adjacents = adjacents[adjacents != None]
-        if unvisited_adjacents.size:
+
+        adjacents = []
+        if x > 0 and cells[x - 1, y] == 0:
+            adjacents.append((x - 1, y))
+        if x < rows - 1 and cells[x + 1, y] == 0:
+            adjacents.append((x + 1, y))
+        if y > 0 and cells[x, y - 1] == 0:
+            adjacents.append((x, y - 1))
+        if y < columns - 1 and cells[x, y + 1] == 0:
+            adjacents.append((x, y + 1))
+
+        if len(adjacents) != 0:
             stack.put((x, y))
 
-            neighbour = random.choice(unvisited_adjacents)
-            neighbour_on_img = np.array(neighbour) * 2 + 1
-            current_on_img = np.array((x, y)) * 2 + 1
-            wall_to_remove = np.mean((neighbour_on_img, current_on_img), axis=0)
-            
-            pixels[tuple(current_on_img.tolist())] = (255, 255, 255)
-            pixels[tuple(wall_to_remove.tolist())] = (255, 255, 255)
-            pixels[tuple(neighbour_on_img.tolist())] = (255, 255, 255)
+            neighbour = choice(adjacents)
+            neighbour_on_img = (neighbour[0] * 2 + 1, neighbour[1] * 2 + 1)
+            current_on_img = (x * 2 + 1, y * 2 + 1)
+            wall_to_remove = (neighbour[0] + x + 1, neighbour[1] + y + 1)
+
+            pixels[neighbour_on_img] = (255, 255, 255)
+            pixels[current_on_img] = (255, 255, 255)
+            pixels[wall_to_remove] = (255, 255, 255)
 
             cells[neighbour] = 1
             stack.put(neighbour)
