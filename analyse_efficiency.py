7992ca40c2cb3efa660e32fa51a7ab4fa1ac1872
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-14 20:34:00 -0800

    Improve plot label
    Added s to indicate the unit of time.

--- analyse_efficiency.py
+++ analyse_efficiency.py
@@ -4,21 +4,23 @@ This file analyses the efficiency of the MAZE GENERATION algorithm.
 
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 from maze_generator import generate_maze
 
-sizes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400]
-times = []
+if __name__ == "__main__":
+    sizes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400]
+    times = []
 
-for size in sizes:
-    start_time = time.time()
+    for size in sizes:
+        start_time = time.time()
 
-    generate_maze(size, size)
-    end_time = time.time() - start_time
-    times.append(end_time)
+        image = generate_maze(size, size)
+        end_time = time.time() - start_time
+        times.append(end_time)
 
-plt.xlabel('Maze Size')
-plt.ylabel('Time Taken')
-plt.title('Time Taken to Generate Mazes')
-plt.plot(sizes, times)
-plt.show()
+    plt.xlabel('Maze Size')
+    plt.ylabel('Time Taken (s)')
+    plt.title('Time Taken to Generate Mazes in seconds.')
+    plt.plot(sizes, times)
+    plt.show()
